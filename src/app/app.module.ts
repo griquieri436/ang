34d3c8a2d4871d448d55329b6b1173a139269806
Gabriel Riquieri <griquieri@gmail.com>
2022-06-02 19:07:55 -0300

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavegationComponent } from './components/navegation/navegation.component';
+import { FootherComponent } from './components/foother/foother.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { StudiesComponent } from './components/studies/studies.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorksComponent } from './components/works/works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavegationComponent,
+    FootherComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    StudiesComponent,
+    SkillsComponent,
+    WorksComponent,
   ],
   imports: [
     BrowserModule,
